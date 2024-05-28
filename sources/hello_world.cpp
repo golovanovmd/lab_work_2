bc0ab6b03ba5a1cf28da86842273f27dbde33583
golovanovmd <maksekos20@gmail.com>
2024-05-28 02:32:09 -0400

    hello_world.cpp changed

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 
 int main () {
-  cout << "Hello world!" << endl;
+  string dat;
+  cin >> dat;
+  cout << "Hello world from " << dat << endl;
   return 0;
 }
